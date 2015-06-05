95057dd55b57326d04576039d27ff54979e5f5fe
Jason Strimpel <jstrimpel@gmail.com>
2015-06-05 11:22:19 -0700

    added dist

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,8 @@ gulp.task('build-example', function () {
     return gulp.src('index.js')
         .pipe(gulp.dest('example/app/lazo-react-view'));
 });
+
+gulp.task('build-dist', function () {
+    return gulp.src('index.js')
+        .pipe(gulp.dest('dist/app/views/lazo-react-view'));
+});
